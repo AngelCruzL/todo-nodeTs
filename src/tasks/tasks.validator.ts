43186f69111cfafa78817d90a6be977d46d0a187
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-03-25 19:02:39 -0600

    Add Task validations

---
+++ src/tasks/tasks.validator.ts
@@ -0,0 +1,44 @@
+import { body, ValidationChain } from 'express-validator';
+
+import { Priority } from '../enums/Priority';
+import { Status } from '../enums/Status';
+
+export const createValidator: ValidationChain[] = [
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title is required')
+    .trim()
+    .isString()
+    .withMessage('Title must be a string'),
+
+  body('date')
+    .not()
+    .isEmpty()
+    .withMessage('Date is required')
+    .isString()
+    .withMessage('Date must be a string'),
+
+  body('description')
+    .trim()
+    .isString()
+    .withMessage('Description must be a text'),
+
+  body('priority')
+    .trim()
+    .isIn([Priority.low, Priority.normal, Priority.high])
+    .withMessage(
+      'Priority can only be low, normal or high',
+    ),
+
+  body('status')
+    .trim()
+    .isIn([
+      Status.todo,
+      Status.inProgress,
+      Status.completed,
+    ])
+    .withMessage(
+      'Status can only be Todo, In Progress or Completed',
+    ),
+];
