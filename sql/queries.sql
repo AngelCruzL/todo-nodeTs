6bc5d33091a86fe16e3de98f88be52b94fc1e0f5
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-03-25 20:51:57 -0600

    Add starter scripts file

---
+++ sql/queries.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS todo_app;
+
+INSERT INTO task (id,
+                  title,
+                  description,
+                  date)
+VALUES ('7a425619-5cf4-4401-b1ff-5b188af1d7c0',
+        'Learn SQL',
+        'Need to improve some abilities with the stored procedures, and some advanced joins',
+        '2023-03-03');
