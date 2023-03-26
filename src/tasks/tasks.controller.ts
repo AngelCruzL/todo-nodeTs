b81e17bb8aee27b8bfd699feb0c4f595ed9695f3
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-03-25 20:25:47 -0600

    Add controller to create new task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { instanceToPlain } from 'class-transformer';
+import { validationResult } from 'express-validator';
 
 import { AppDataSource } from '../index';
 import { Task } from './tasks.entity';
@@ -25,6 +26,46 @@ class TasksController {
       allTasks = instanceToPlain(allTasks) as Task[];
 
       return res.status(200).json(allTasks);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        message: 'Something went wrong',
+      });
+    }
+  }
+
+  public async create(
+    req: Request,
+    res: Response,
+  ): Promise<Response> {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res
+        .status(422)
+        .json({ errors: errors.array() });
+    }
+
+    const { title, date, description, priority, status } =
+      req.body;
+    const newTask = new Task();
+    newTask.title = title;
+    newTask.date = date;
+    newTask.description = description;
+    newTask.priority = priority;
+    newTask.status = status;
+
+    let createdTask: Task;
+
+    try {
+      createdTask = await AppDataSource.getRepository(
+        Task,
+      ).save(newTask);
+
+      // Convert the task instance to an object
+      createdTask = instanceToPlain(createdTask) as Task;
+
+      return res.status(201).json(createdTask);
     } catch (err) {
       console.log(err);
       return res.status(500).json({
--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,5 +1,4 @@
-import { Request, Response, Router } from 'express';
-import { validationResult } from 'express-validator';
+import { Router } from 'express';
 
 import { tasksController } from './tasks.controller';
 import { createValidator } from './tasks.validator';
@@ -11,16 +10,5 @@ tasksRouter.get('/tasks', tasksController.getAll);
 tasksRouter.post(
   '/tasks',
   createValidator,
-  //@ts-ignore
-  async (req: Request, res: Response) => {
-    console.log(req.body);
-    const errors = validationResult(req);
-    // console.log(errors);
-
-    if (!errors.isEmpty()) {
-      return res
-        .status(422)
-        .json({ errors: errors.array() });
-    }
-  },
+  tasksController.create,
 );
